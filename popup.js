9feabb5a09eef086767ecaeeb0167f985a0ebceb
torrey.blog <torreyblog@protonmail.com>
2019-04-14 13:23:55 -0700

    Many style changes and content additions

--- popup.js
+++ popup.js
@@ -1,10 +1,25 @@
-function handleClick(e) {
-	var sending = chrome.runtime.sendMessage({
-	    url: e.target.href
-  	})
+function handleClickButton(e) {
+  if (typeof e.target.value !== 'undefined' && e.target.value != "") {
+  	chrome.runtime.sendMessage({
+  	    url: e.target.value
+    	})
+  }
 }
 
+function handleClickAnchor(e) {
+  if (typeof e.target.href !== 'undefined' && e.target.href != "") {
+    chrome.runtime.sendMessage({
+        url: e.target.href
+      })
+  }
+}
+
+var buttons = document.querySelectorAll('button.dapplink')
+buttons.forEach(function(button) {
+	addEventListener('click', handleClickButton)
+})
+
 var anchors = document.querySelectorAll('a')
-anchors.forEach(function(anchor) {
-	addEventListener('click', handleClick)
+anchors.forEach(function(button) {
+  addEventListener('click', handleClickAnchor)
 })
